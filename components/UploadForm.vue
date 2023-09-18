c84836c231ba904c122c1833d7e2a72ec7996301
patrickm4 <patrickmoreno444@gmail.com>
2023-09-17 20:51:23 -0700

    master style: tailwind inputs, spacing, text, feat: display items if table number present, display file upload if items checked

--- components/UploadForm.vue
+++ components/UploadForm.vue
@@ -1,21 +1,70 @@
 <template>
-    <div class="mt-2">
-        <label>Table number</label>
-        <input type="text"/>
+    <div class="mt-2 flex flex-col">
+        <!-- <label>Table number</label>
+        <input type="text"/> -->
 
-        <label>Upload photo</label>
-        <input type="file"/>
+        <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-2xl">
+        <form class="space-y-6" action="#" method="POST">
 
-        <div>
-            <label>Take a table selfie with ...</label>
-            <ul>
-                <li>
-                    Something Blue
-                </li>
-                <li>
-                    Someone with tattoos
-                </li>
-            </ul>
+            <div>
+                <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Table Number</label>
+                <div class="mt-2">
+                    <input v-model="tableNumber" id="email" name="email" type="email" autocomplete="email" required class="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                </div>
+            </div>
+
+            <div v-if="tableNumber">
+                <label for="email" class="block text-sm font-medium leading-6 text-gray-900 mb-2">Take a table selfie with ...</label>
+                <ul>
+                    <li 
+                        v-for="item in huntItems"
+                        class="relative flex gap-x-3"
+                    >
+                        <div class="flex h-6 items-center">
+                            <input id="comments" name="comments" type="checkbox" class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                            v-model="objectives"
+                            :value="item"
+                            >
+                        </div>
+                        {{ item }}
+                    </li>
+                </ul>
+            </div>
+
+            <div v-if="tableNumber && objectives?.length > 0">
+                <label for="email" class="block text-sm font-medium leading-6 text-gray-900 mb-2">Upload Photo</label>
+                <input type="file" class="block w-full text-sm text-slate-500
+                file:mr-4 file:py-2 file:px-4
+                file:rounded-full file:border-0
+                file:text-sm file:font-semibold
+                file:bg-violet-50 file:text-violet-700
+                hover:file:bg-violet-100
+                "/>
+            </div>
+
+            <div>
+                <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" :disabled="true">Submit</button>
+            </div>
+
+            </form>
         </div>
     </div>
 </template>
+
+
+<script lang="ts">
+
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  // type inference enabled
+  data() {
+    return {
+      tableNumber: 0,
+      objectives: [],
+      huntItems: ["Something blue", "Someone with Tattoos"]
+    }
+  }
+})
+
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,9 @@
 <template>
-    <div>
+    <div class="px-10 mt-28">
         <NuxtLink to="/pictures">Pictures</NuxtLink>
-        <p class="text-base font-semibold leading-7 text-indigo-600">Welcome to Dorothy and Patrick's</p>
+        <p class="text-base font-semibold leading-7 text-indigo-600 mt-5">Welcome to Dorothy and Patrick's</p>
         <h1 class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Scavenger Hunt!</h1>
-        <p class="mt-6 text-xl leading-8 text-gray-700">Aliquet nec orci mattis amet quisque ullamcorper neque, nibh sem. At arcu, sit dui mi, nibh dui, diam eget aliquam. Quisque id at vitae feugiat egestas.</p>
+        <p class="mt-6 text-xl leading-8 text-gray-700">Take a table selfie with each item or person then upload it here.</p>
         <UploadForm />
     </div>
 </template>
