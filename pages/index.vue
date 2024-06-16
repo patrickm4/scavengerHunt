ba8e74df3b7e1eec7617e25a20a6066db0904178
Patrick Moreno <patrickmoreno444@gmail.com>
2024-06-16 11:49:23 -0700

    staging feat: allow download when previewing uploading image

--- pages/index.vue
+++ pages/index.vue
@@ -101,15 +101,21 @@
             loading
           </div>
           <div v-else class="flex thumbnail-container">
-            <img
-              v-for="photo in photos"
-              class="mr-2 photo-thumbnail"
-              :src="photo.fileb64String"
-              :alt="photo.name"
-              height="250"
-              width="auto"
-              @click="previewPhotoToInspect = photo"
-            />
+            <div v-for="photo in photos" class="mr-2 ">
+              <img
+                class="photo-thumbnail"
+                :src="photo.fileb64String"
+                :alt="photo.name"
+                height="250"
+                width="auto"
+                @click="previewPhotoToInspect = photo"
+              />
+              <!-- <a v-if="photo.href" :href="photo.href" class="mt-5 flex w-full justify-center rounded-md bg-red-300 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm enabled:hover:bg-red-200 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:opacity-30" download>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
+                </svg>
+              </a> -->
+            </div>
           </div>
         </div>
 
@@ -176,6 +182,10 @@ export default defineComponent({
       ],
       objectives: [],
       selectedTask: "",
+      // hrefs: [],
+      href: '',
+
+
     };
   },
   async mounted() {
@@ -245,6 +255,7 @@ export default defineComponent({
     },
     setPhoto(event: Event) {
       this.filesLength = event.target.files.length || 0;
+
       this.photos = [];
 
       if ((event.target as HTMLInputElement)?.files) {
@@ -264,6 +275,7 @@ export default defineComponent({
               name: file.name,
               type: file.type,
               size: file.size,
+              href: URL.createObjectURL(file),
             });
           };
         }
